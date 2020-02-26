5b6bf0e9b0ecffe7d3ee09d8a5693d5113bee73d
Ekki Lu <ekki@EkkideMacBook-Pro.local>
2020-02-26 11:18:52 -0800

    added getNRandom

--- gitpracticeEKKI.py
+++ gitpracticeEKKI.py
@@ -2,9 +2,16 @@
 # Partner 2 Name: Clyde Beuter
 ###############################
 # Assignment Name: GitHub Practice - 2/26/20
+
+import random as rand
+
 def getNRandom(n):
 	'''takes in an integer and returns a list of n random integers between 1 and 10, inclusive'''
-    pass
+    n_list = []
+    for i in range(n):
+        n_list.append(rand.randint(1,10))
+    return n_list
+
 
 def multiplyRandom(numbers):
         '''takes in a list of n numbers and returns the product of the numbers'''
